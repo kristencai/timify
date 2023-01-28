1855c28cc83c88fa1cc6bb85041949b6c22b42ff
“syu0916” <sean_yu@brown.edu>
2023-01-28 17:04:46 -0500

    styling page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,34 @@ import LogIn from './backend/auth';
 function App() {
   return (
     <div className="App">
-      <div style={{flex: "flex", flexDirection:"row"}}>
-        <div>
+      <div className = "homeContainer">
+        <div className = "left">
           <LogIn/>
         </div>
-        <div>
-            asdfasdf
+        <div className = "right">
+          <div className = "black">
+
+          </div>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/50.-Taylor-Swift-%E2%80%981989-2014-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/media/Young-Thug-Jeffery-2016-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/48.-Lady-Gaga-%E2%80%98The-Fame-Monster-2009-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/46.-Funkadelic-%E2%80%98Maggot-Brain-1971-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/media/cardi-b-invasion-of-privacy-album-art-2018-billboard-embed.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/44.-Whitney-Houston-%E2%80%98Whitney-Houston-1985-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/43.-Fleetwood-Mac-%E2%80%98Rumours-1977-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/42.-Nicki-Minaj-%E2%80%98The-Pinkprint-2014-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/41.-No-Doubt-%E2%80%98Tragic-Kingdom-1995-album-art-billbooard-1240.jpg?w=1026"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/40.-Beyonce-%E2%80%98Beyonce-2013-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/media/ariana-grande-sweetner-album-art-2018-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/33.-Kanye-West-%E2%80%98My-Beautiful-Dark-Twisted-Fantasy-2010-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/media/Lizzo-Cuz-I-Love-You-album-art-2019-billboard-1240_0.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/30.-FKA-Twigs-%E2%80%98LP1-2014-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/29.-Aretha-Franklin-%E2%80%98I-Never-Loved-a-Man-the-Way-I-Loved-You-1967-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/28.-Bruce-Springsteen-%E2%80%98Born-in-the-U.S.A.-1984-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/22.-Drake-If-Youre-Reading-This-Its-Too-Late-2015-album-art-billboard-1240.jpg?w=1024"/>
+            <img className = "collage" src = "https://www.billboard.com/wp-content/uploads/2022/03/16.-The-Clash-%E2%80%98London-Calling-1979-album-art-billboard-1240.jpg?w=1030"/>
+
+            
         </div>
       </div>
 
